a348932d694b693e90d0069b9bd97bdb03699837
Rnveer007 <rvs523474@gmail.com>
2025-04-02 15:35:14 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import groot from "./assets/groot.png";
-import ironman from "./assets/ironman.jpg";
+import hulk from "./assets/hulk.png";
 import thor from "./assets/thor.png";
 
 function App() {
@@ -8,7 +8,8 @@ function App() {
   const [selectedAvatar, setSelectedAvatar] = useState("");
   const [score, setScore] = useState(0);
   const [generatedImages, setGeneratedImages] = useState([]);
-  console.log(generatedImages)
+  const [timer, setTimer] = useState(60)
+  // console.log(score)
 
   function handleClick(src) {
     setSelectedAvatar(src);
@@ -19,6 +20,17 @@ function App() {
     let interval;
     if (screen === 3 && selectedAvatar) {
       interval = setInterval(() => {
+
+        setTimer(prevTime => {
+          if (prevTime === 0) {
+            clearInterval(interval);
+            setGeneratedImages([]);
+            return 0
+          }
+
+          return prevTime - 1;
+        }
+        )
         setGeneratedImages((prevImages) => [
           ...prevImages,
           {
@@ -38,6 +50,10 @@ function App() {
       : Math.floor(Math.random() * 400);
   }
 
+  function handleDelete(id) {
+    setGeneratedImages(generatedImages.filter((item) => item.id !== id))
+    setScore(prevScore => prevScore + 1);
+  }
   return (
     <div className="h-screen flex items-center justify-center relative">
       {/* Screen 1: Start Game */}
@@ -55,22 +71,22 @@ function App() {
       {screen === 2 && (
         <div className="screen2">
           <h3 className="text-center my-5 font-bold">Select Your Avatar</h3>
-          <div className="avatar-wrapper flex justify-center gap-10 pt-10">
+          <div className="avatar-wrapper flex justify-center gap-20 pt-10">
             <img
               onClick={(e) => handleClick(e.target.src)}
-              className="avatar w-[200px] cursor-pointer"
+              className="avatar w-[200px] cursor-pointer object-cover"
               src={groot}
               alt="GROOT"
             />
             <img
               onClick={(e) => handleClick(e.target.src)}
-              className="avatar w-[200px] cursor-pointer"
-              src={ironman}
-              alt="IRONMAN"
+              className="avatar w-[200px] cursor-pointer object-cover"
+              src={hulk}
+              alt="HULK"
             />
             <img
               onClick={(e) => handleClick(e.target.src)}
-              className="avatar w-[200px] cursor-pointer"
+              className="avatar w-[200px] cursor-pointer object-cover"
               src={thor}
               alt="THOR"
             />
@@ -82,17 +98,18 @@ function App() {
       {screen === 3 && (
         <div className="screen3 absolute top-10 left-10">
           <div className="info">
-            <p>Time Left: <span></span></p>
-            <p>Score: <span>{score}</span></p>
+            <p>Time Left: <span className="ml-3">{timer}</span></p>
+            <p>Score: <span className="ml-3">{score}</span></p>
           </div>
           <div className="playingArea mt-10">
             {generatedImages.map((image, index) => (
               <img
-                className="killShot w-[150px]"
+                className="killShot w-[100px] my-10 absolute cursor-pointer"
                 key={index}
                 src={selectedAvatar}
                 alt="IMAGE"
                 style={{ left: image.x, top: image.y }}
+                onClick={() => handleDelete(image.id)}
               />
             ))}
           </div>
